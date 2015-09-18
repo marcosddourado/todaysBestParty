a8913d6acbda2ed4377d5d56715f62df6eb30769
marcosddourado <marcos_bdrlr@hotmail.com>
2015-09-18 00:57:20 -0700

    inserting project

---
+++ BestPartyToday/app/src/main/java/unlv/com/bestpartytoday/MapsActivity.java
@@ -0,0 +1,47 @@
+package unlv.com.bestpartytoday;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+public class MapsActivity extends FragmentActivity  implements LocationListener{
+
+    protected LocationManager locationManager;
+    protected LocationListener locationListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sfvehicles);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) this);
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        Log.i("Latitude:", String.valueOf(location.getLatitude()));
+        Log.i("Longitude:" , String.valueOf(location.getLongitude()));
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Log.d("Latitude", "disable");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Log.d("Latitude","enable");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        Log.d("Latitude", "status");
+    }
+
+}
