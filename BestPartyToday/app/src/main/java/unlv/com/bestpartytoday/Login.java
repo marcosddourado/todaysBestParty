553093f9e251a8d54c5885e19fefa1bdb809d0a6
marcosddourado <marcos_bdrlr@hotmail.com>
2015-11-22 23:20:38 -0700

    Fixing bug on the login layout.

--- BestPartyToday/app/src/main/java/unlv/com/bestpartytoday/Login.java
+++ BestPartyToday/app/src/main/java/unlv/com/bestpartytoday/Login.java
@@ -2,7 +2,6 @@ package unlv.com.bestpartytoday;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class Login extends AppCompatActivity {
 
@@ -10,9 +9,6 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
     }
 
 }
